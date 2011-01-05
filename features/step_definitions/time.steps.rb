79a96570366a1718d530dca9b05ef58cb4ef8545
Franklin Webber <franklin.webber@gmail.com>
2011-01-05 13:27:39 -0800

    FIX: Inclusive Around Time
      - Inclusive value was not passing the inclusive to the within operation
        so inclusive state was being lost on the subsequent call.  Added
        the term that fixed it.
    
      + Also made the steps use true or false if specified.  inclusive
        defaults to false when not specified.

--- features/step_definitions/time.steps.rb
+++ features/step_definitions/time.steps.rb
@@ -4,26 +4,26 @@ When /^searching the log for the time (.+)$/ do |time|
 end
 
 When /^searching the log for all entries after( and including)? the time (.+)$/ do |inclusive,time|
-  @results = @indy.after(:time => time, :inclusive => inclusive).for(:all)
+  @results = @indy.after(:time => time, :inclusive => (inclusive ? true : false)).for(:all)
 end
 
 When /^searching the log for all entries before( and including)? the time (.+)$/ do |inclusive,time|
-  @results = @indy.before(:time => time, :inclusive => inclusive).for(:all)
+  @results = @indy.before(:time => time, :inclusive => (inclusive ? true : false)).for(:all)
 end
 
 When /^searching the log for all entries between( and including)? the times? (.+) and (.+)$/ do |inclusive,start,stop|
-  @results = @indy.within(:time => [start,stop], :inclusive => inclusive).for(:all)
+  @results = @indy.within(:time => [start,stop], :inclusive => (inclusive ? true : false)).for(:all)
 end
 
 When /^searching the log for all entries (\d+) minutes around( and including)? the time (.+)$/ do |time_span,inclusive,time|
-  @results = @indy.around(:time => time, :span => time_span, :inclusive => inclusive).for(:all)
+  @results = @indy.around(:time => time, :span => time_span, :inclusive => (inclusive ? true : false)).for(:all)
 end
 
 When /^searching the log for all entries (\d+) minutes after( and including)? the time (.+)$/ do |time_span,inclusive,time|
-  @results = @indy.after(:time => time, :span => time_span, :inclusive => inclusive).for(:all)
+  @results = @indy.after(:time => time, :span => time_span, :inclusive => (inclusive ? true : false)).for(:all)
 end
 
 When /^searching the log for all entries (\d+) minutes before( and including)? the time (.+)$/ do |time_span,inclusive,time|
-  @results = @indy.before(:time => time, :span => time_span, :inclusive => inclusive).for(:all)
+  @results = @indy.before(:time => time, :span => time_span, :inclusive => (inclusive ? true : false)).for(:all)
 end
 
