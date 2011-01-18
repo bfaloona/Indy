25540fe04a0a6a1a9c2dc111e0e0b4e0eb69c8d5
Brandon Faloona <bfaloona@globys.com>
2011-01-17 18:31:12 -0800

    OpenStruct is slooooooow... profiling tests show.

--- performance/profile_spec.rb
+++ performance/profile_spec.rb
@@ -2,52 +2,29 @@ require "#{File.dirname(__FILE__)}/helper"
 
 describe "Search Performance" do
 
-
-  context "with a 420 line log file" do
-
-    log_content = [
-      "2000-09-07 14:07:41 INFO  MyApp - Entering application.\n",
-      "2000-09-07 14:07:42 DEBUG MyApp - Focusing application.\n",
-      "2000-09-07 14:07:43 DEBUG MyApp - Blurring application.\n",
-      "2000-09-07 14:07:44 WARN  MyApp - Low on Memory.\n",
-      "2000-09-07 14:07:45 ERROR MyApp - Out of Memory.\n",
-      "2000-09-07 14:07:46 INFO  MyApp - Exiting application.\n"
-    ].collect {|line| line * 70 }.join
-
-    profile :file => STDOUT, :printer => :flat, :min_percent => 1  do
-
-      it "should profile code using #for(:all)" do
-        Indy.search(log_content.dup).for(:all)
-      end
-
-      it "should profile code using #for(:field => 'value')" do
-        Indy.search(log_content.dup).for(:severity => 'INFO')
-      end
-
-      it "should profile code using #time()" do
-        Indy.search(log_content.dup).after(:time => "2000-09-07 14:07:45").for(:all)
-      end
-
-    end
-
-  end
-
   context "with a 10000 line log file" do
 
     large_file = "#{File.dirname(__FILE__)}/large.log"
+    before(:all) do
+      @indy = Indy.search(large_file).with([/^\[([^\|]+)\|([^\]]+)\] (.*)$/,:severity, :time, :message])
+    end
 
     profile :file => STDOUT, :printer => :flat, :min_percent => 1  do
 
       it "should profile code using #for(:all)" do
-        Indy.search(large_file).for(:all)
+        @indy.for(:all)
       end
 
       it "should profile code using #for(:field => 'value')" do
-        Indy.search(large_file).for(:severity => 'DEBUG')
+        @indy.for(:severity => 'DEBUG')
+      end
+
+      it "should profile code using #like(:field => 'value')" do
+        @indy.like(:message => 'filesystem')
       end
 
       it "should profile code using #time()" do
-        Indy.search(large_file).after(:time => "12-29-2010 12:11:33").for(:all)
+        @indy.after(:time => "12-29-2010 12:11:33").for(:all)
       end
 
     end
