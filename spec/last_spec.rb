5f1de6a3c28dcf6b50db72478abf74a292e1c77e
Brandon Faloona <brandon@faloona.net>
2011-01-22 11:32:08 -0800

    Initial commit of #last, #last_entry, and #last_entries

--- spec/last_spec.rb
+++ spec/last_spec.rb
@@ -6,38 +6,32 @@ describe 'Indy#last' do
 
     before(:each) do
       log_string = ["2000-09-07 14:07:41 INFO  MyApp - Entering APPLICATION.",
-                    "2000-09-08 14:08:41 INFO  MyOtherApp - Exiting APPLICATION.",
-                    "2000-09-09 14:07:00 INFO  MyOtherApp - Exiting APPLICATION.",
+                    "2000-09-07 14:09:00 INFO  MyApp - Entering APPLICATION.",
+                    "2000-09-09 14:10:56 INFO  MyOtherApp - Exiting APPLICATION.",
                     "2000-09-10 14:08:00 INFO  MyOtherApp - Exiting APPLICATION.",
+                    "2000-09-10 14:08:41 INFO  MyOtherApp - Exiting APPLICATION.",
                     "2000-09-10 14:09:00 INFO  MyOtherApp - Exiting APPLICATION.",
                     "2000-09-10 14:09:30 INFO  MyOtherApp - Exiting APPLICATION.",
                     "2000-09-10 14:10:55 INFO  MyApp - Exiting APPLICATION."].join("\n")
       @indy = Indy.search(log_string)
     end
 
-    it "should be a method" do
-      @indy.should.respond_to?( :last )
-      puts 'bad test!!'
-      puts @indy.last
-    end
-
     it "should return self" do
-      @indy.last(1).should be_kind_of? Indy
+      @indy.last(:span => 1).should be_kind_of Indy
     end
 
     it "should raise an error if passed an invalid parameter" do
       lambda{ @indy.last('a') }.should raise_error( ArgumentError )
       lambda{ @indy.last() }.should raise_error( ArgumentError )
       lambda{ @indy.last(nil) }.should raise_error( ArgumentError )
-      lambda{ @indy.last([]) }.should raise_error( ArgumentError )
-    end
-
-    it "should return correct number of rows when passed a number" do
-      @indy.last( 3 ).for(:all).length.should == 3
+      lambda{ @indy.last({}) }.should raise_error( ArgumentError )
     end
 
     it "should return correct number of rows when passed a span of minutes" do
       @indy.last( :span => 2).for(:all).length.should == 3
+      @indy.last( :span => 3).for(:all).length.should == 5
+      @indy.last( :span => 1_440).for(:all).length.should == 6
+      
     end
 
     it "should return correct rows when passed a span of minutes" do
