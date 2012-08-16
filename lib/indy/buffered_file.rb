ced8d620c62ac9458c262a4bac63eb236c8e3a03
Brandon Faloona <brandon@faloona.net>
2012-08-16 13:08:55 -0400

    Simplifiy #[] and #load_buffer

--- lib/indy/buffered_file.rb
+++ lib/indy/buffered_file.rb
@@ -25,21 +25,8 @@ class BufferedFile
     return reverse_entry_seek_index(entry_seek_index) if entry_seek_index < 0
     buffer_entry_index = 0
     @buffer_byte_offset = 0
-    while entry_seek_index >= buffer_entry_index
-      #puts "entry_seek_index: #{entry_seek_index}"
-      #puts "buffer_entry_index: #{buffer_entry_index}"
-      at_eof = load_buffer
-      @buffer_entries = split_entries @buffer
-      unless at_eof
-        #puts "!eof"
-        # drop trailing entry since it's partial
-        last_entry_size = @buffer_entries.last.length
-        @buffer_entries = @buffer_entries[0..-2]
-        @buffer_byte_offset -= last_entry_size
-      end
-      buffer_entry_index += @buffer_entries.size
-      #puts "buffer_entry_index: #{buffer_entry_index}"
-      #puts "buffer_entries: #{@buffer_entries}"
+    while entry_seek_index >= buffer_entry_index do
+      buffer_entry_index += load_buffer
     end
     entry_index_within_buffer = -(buffer_entry_index - entry_seek_index)
     #puts "entry_index_within_buffer: #{entry_index_within_buffer}"
@@ -50,17 +37,8 @@ class BufferedFile
   def reverse_entry_seek_index(entry_seek_index)
     buffer_entry_index = 0
     @buffer_byte_offset = @file.size
-    while entry_seek_index.abs > buffer_entry_index.abs
-      #puts "entry_seek_index: #{entry_seek_index}"
-      #puts "buffer_entry_index: #{buffer_entry_index}"
-      load_buffer :reverse
-      @buffer_entries = split_entries @buffer
-      first_entry_size = @buffer_entries.first.length
-      @buffer_entries = @buffer_entries.drop(1)
-      @buffer_byte_offset += first_entry_size
-      buffer_entry_index -= @buffer_entries.size
-      #puts "buffer_entry_index: #{buffer_entry_index}"
-      #puts "buffer_entries: #{@buffer_entries}"
+    while entry_seek_index < buffer_entry_index do
+      buffer_entry_index -= load_buffer :reverse
     end
     entry_index_within_buffer = (buffer_entry_index.abs - entry_seek_index.abs)
     #puts "entry_index_within_buffer: #{entry_index_within_buffer}"
@@ -69,29 +47,24 @@ class BufferedFile
   end
 
   def load_buffer(reverse=false)
-    if reverse
-      file.open.rewind
-      reverse_seek_offset = @buffer_byte_offset - @buffer_size
-      #puts "reverse_seek_offset: #{reverse_seek_offset}"
-      @file.seek(reverse_seek_offset)
-      @buffer = @file.read(@buffer_size)
-      #puts "load_buffer - @buffer: #{@buffer.inspect}"
-      @buffer_byte_offset -= @buffer.length
-      # inform caller if end of file was reached by returning true
-      is_eof = true if @file.eof?
-      @file.close
-      return is_eof
-    else
-      file.open.rewind
-      file.seek(@buffer_byte_offset)
-      @buffer = file.read(@buffer_size)
-      #puts "load_buffer - @buffer: #{@buffer.inspect}"
-      @buffer_byte_offset += @buffer.length
-      # inform caller if end of file was reached by returning true
-      is_eof = true if @file.eof?
-      file.close
-      return is_eof
+    @file.open.rewind
+    #puts "reverse_seek_offset: #{reverse_seek_offset}"
+    reverse ? @file.seek(@buffer_byte_offset - @buffer_size) : @file.seek(@buffer_byte_offset)
+    @buffer = @file.read(@buffer_size)
+    #puts "load_buffer - @buffer: #{@buffer.inspect}"
+    buffer_size = @buffer.length
+    reverse ? @buffer_byte_offset -= buffer_size : @buffer_byte_offset += buffer_size
+    # inform caller if end of file was reached by returning true
+    @buffer_entries = split_entries @buffer
+    unless @file.eof?
+      #puts "!eof"
+      # drop trailing entry since it's partial
+      last_entry_size = @buffer_entries.last.length
+      @buffer_entries = reverse ? @buffer_entries.drop(1) : @buffer_entries[0..-2]
+      @buffer_byte_offset -= last_entry_size
     end
+    @file.close
+    @buffer_entries.size
   end
 
   def split_entries(data)
