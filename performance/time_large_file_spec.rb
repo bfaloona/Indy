4d9bd08e6833a7649ceaeb58752bc26ad527cb45
Brandon Faloona <brandon@faloona.net>
2012-10-06 08:41:33 -0700

    Performance spec typo and output

--- performance/time_large_file_spec.rb
+++ performance/time_large_file_spec.rb
@@ -1,15 +1,17 @@
 require 'rubygems'
 require 'indy'
 
-describe '5000 entry file' do
-  it 'should find 130 entries using within' do
+describe '50000 entry file' do
+  it 'should find 130 entries using #within time scope' do
     large_file = File.open("#{File.dirname(__FILE__)}/large.log", 'r')
 
+    start_time = Time.now
     indy = Indy.new(
       :source => large_file,
       :log_format => [/^\[([^\|]+)\|([^\]]+)\] (.*)$/,:severity, :time, :message])
 
     result = indy.within(:time => ["27-12-2010 10:14:25","29-12-2010 12:10:19"]).for(:all)
+    puts "#{(Time.now - start_time).seconds} \tElapsed seconds to parse 50k line file for all entries in time scope"
     result.size.should == 130
   end
 end
